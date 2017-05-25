bccbc49e2385f158d7cf3bd59850ce02120d6b4b
lilingang <lilingang@huami.com>
2017-05-25 10:03:06 +0800

    ref |> 新建工程

---
+++ HMLogger/Mars/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "de1982c"
+#define MARS_PATH "custom"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2017-05-24 19:42:36"
+#define MARS_TAG "tag_[2017-05-24_19.42]@git-de1982c@lilingang"
+
+#endif
---
+++ HMLoggerDemo/Pods/Target Support Files/Pods-HMLoggerDemo/Pods-HMLoggerDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HMLoggerDemo : NSObject
+@end
+@implementation PodsDummy_Pods_HMLoggerDemo
+@end
